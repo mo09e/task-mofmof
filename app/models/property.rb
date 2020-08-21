c8f532ab9366431971aea323370bfbd4be179210
Moe <moe_akahane@diveintocode.jp>
2020-08-21 16:29:48 +0900

    controllerのパラメータの記述を修正

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
-  accepts_nested_attributes_for :nearest_stations, allow_destroy: true, reject_if: :reject_both_blank
+  accepts_nested_attributes_for :nearest_stations, allow_destroy: true,reject_if: :reject_both_blank
 
   validates :name, presence: true
   validates :rent, presence: true
