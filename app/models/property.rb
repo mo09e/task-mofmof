7d61286ce2197572bbe702e898a48bdba189a6b9
Moe <moe_akahane@diveintocode.jp>
2020-08-21 11:38:40 +0900

    propertyモデルとnearest_stationモデルにバリデーションを定義

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,10 @@
 class Property < ApplicationRecord
+  has_many :nearest_station, dependent: :destroy
+  accepts_nested_attributes_for :nearest_stations, allow_destroy: true, reject_if: :all_blank
+
+  validates :name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :remarks, presence: true
 end
